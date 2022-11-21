7a8531ec54874e20ed89ea4d47eaace2fd7461bf
Fedor4994 <fedorsosnin04@gmail.com>
2022-11-21 09:13:58 +0200

    Create project

---
+++ src/components/App.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+export class App extends Component {
+  render() {
+    return (
+      <div
+        style={{
+          height: '100%',
+          padding: 10,
+        }}
+      >
+        hello world
+      </div>
+    );
+  }
+}
